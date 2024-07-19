4324a7dc1fbc77e05c274baaa064e1172d8a0691
Blackbrood <96950354+Blackbrood@users.noreply.github.com>
2024-07-19 08:23:17 -0700

    Bug-Geser

---
+++ BACA.js
@@ -0,0 +1,26 @@
+
+// Kalo mau Ambil Code Atau recode Boleh aja Tapi izin dulu kontol
+// TELEGRAM DITZZ EMG GA ADA TAPI NOMER WhatsappNYA ADA 
+/* Reupload Atau Review Bot 
+ * 1. Tag me @TrepxC2
+ * 2. Tag me @callmevinz
+ * 3. Tag me @wnnStarJoyy
+ * 4. Tag me @kayyoffc
+ * 5. Tag me @Yangtaggasik
+ * 6. Tambahkan Saya ke Credits Kalo bisa
+ *\
+ Aku cuma recode aja ya sama ngedit ngedit dikit hehe jngn bully 🤓
+/*         Sedikit Info        */
+// jika kalian butuh apa silahkan chat nomer di bawah
+// McLareN lo warna apa bos :v
+// +48 45 926 63 37 ( BI4BAYOU CLONES )
+// +62 895-0842-805 ( Kayy Dev )
+// +62 ???-????-????? ( -------------------- ) Siapakah Dia?
+// +62 823-2147-832 ( Akmal Mods )
+// +62 811-6226-262 ( WNN STYJOY )
+// +62 856-4008-7504 ( Penggemar Recode )
+
+// SC BERKEMBANG SECARA MANUAL DARI SESEORANG KE SESEORANG UNTUK MEMPERKEMBANG CASE DAN RECODE 
+// SELAMAT MENCOBA 
+
+© Ditzz Project
